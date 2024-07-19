92fc1c06f1d291430bf0f2bbbb9eb0b3a0063f9a
d3v1lcon3 <135651153+d3v1lcon3@users.noreply.github.com>
2024-07-19 16:01:49 -0300

    Update graficos.py correcao

--- graficos.py
+++ graficos.py
@@ -6,26 +6,22 @@ from matplotlib.animation import FuncAnimation
 import os
 from datetime import datetime
 
-# Função para obter o nome do arquivo de log atual
-def get_log_filename(log_type):
+# Função para obter o nome do arquivo de log de performance atual
+def get_performance_log_filename():
     data = datetime.now().strftime('%d-%m-%Y')
-    return f'{log_type}_{data}.csv'
+    return f'performance_{data}.csv'
 
-# Função para ler os logs e atualizar os gráficos
-def update_logs(i, cpu_line, ram_line, disk_line, log_type):
-    log_filename = get_log_filename(log_type)
+# Função para ler os logs de performance e atualizar os gráficos
+def update_logs(i, cpu_line, ram_line, disk_line):
+    log_filename = get_performance_log_filename()
     if os.path.isfile(log_filename):
         df = pd.read_csv(log_filename)
-        if log_type == 'performance':
-            if not df.empty:
-                cpu_line.set_data(df.index, df['CPU'])
-                ram_line.set_data(df.index, df['RAM'])
-                disk_line.set_data(df.index, df['Disk'])
-                ax.relim()
-                ax.autoscale_view()
-        elif log_type == 'processes':
-            # Adicione o código para processar os logs de processos se necessário
-            pass
+        if not df.empty:
+            cpu_line.set_data(df.index, df['CPU'])
+            ram_line.set_data(df.index, df['RAM'])
+            disk_line.set_data(df.index, df['Disk'])
+            ax.relim()
+            ax.autoscale_view()
 
 # Configurar o gráfico
 fig, ax = plt.subplots()
@@ -33,13 +29,13 @@ cpu_line, = ax.plot([], [], label='CPU')
 ram_line, = ax.plot([], [], label='RAM')
 disk_line, = ax.plot([], [], label='Disk')
 
-ax.set_xlim(0, 100)
+ax.set_xlim(0, 100)  # Este limite será ajustado dinamicamente
 ax.set_ylim(0, 100)
-ax.set_xlabel('Time')
-ax.set_ylabel('Usage (%)')
+ax.set_xlabel('Tempo')
+ax.set_ylabel('Uso (%)')
 ax.legend()
 
 # Função de animação para atualizar o gráfico
-ani = FuncAnimation(fig, update_logs, fargs=(cpu_line, ram_line, disk_line, 'performance'), interval=1000)
+ani = FuncAnimation(fig, update_logs, fargs=(cpu_line, ram_line, disk_line), interval=1000)
 
 plt.show()
