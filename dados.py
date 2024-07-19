320fb46e57c93c431653a3b7db82d9e5bb428bf1
d3v1lcon3 <135651153+d3v1lcon3@users.noreply.github.com>
2024-07-19 15:29:49 -0300

    Update dados.py versao 2

--- dados.py
+++ dados.py
@@ -2,38 +2,50 @@
 
 import psutil
 import time
+import relatorio
 
 def list_processes():
-	processes = []
+    processes = []
 
-	for proc in psutil.process_iter(['pid', 'name', 'username', 'cpu_percent', 'memory_percent']):
-		try:
-			process_info = proc.info
-			processes.append(process_info)
-		except (psutil.NoSuchProcess, psutil.AcessDenied, psutil.ZombieProcess):
-			pass
-	return processes
+    for proc in psutil.process_iter(['pid', 'name', 'username', 'cpu_percent', 'memory_percent']):
+        try:
+            process_info = proc.info
+            processes.append(process_info)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return processes
 
 def get_system_usage():
-	cpu_usage = psutil.cpu_percent(interval=1)
+    cpu_usage = psutil.cpu_percent(interval=1)
 
-	memory_info = psutil.virtual_memory()
-	ram_usage = memory_info.percent
+    memory_info = psutil.virtual_memory()
+    ram_usage = memory_info.percent
 
-	disk_usage = psutil.disk_usage('/')
-	disk_percent = disk_usage.percent
+    disk_usage = psutil.disk_usage('/')
+    disk_percent = disk_usage.percent
 
-	return cpu_usage, ram_usage, disk_percent
+    return cpu_usage, ram_usage, disk_percent
 
 if __name__ == "__main__":
-	process_list = list_processes()
+    # Abrir os arquivos CSV
+    relatorio.open_tables()
 
-	while True:
-		cpu, ram, disk = get_system_usage()
-		print(f"Uso da CPU: {cpu}%")
-		print(f"Uso da RAM: {ram}%")
-		print(f"Uso do Disco: {disk}%")
-		for process in process_list:
-			print(f"PID: {process['pid']}, Nome: {process['name']}, Usuario: {process['username']}, CPU: {process['cpu_percent']}, Memoria: {process['memory_percent']}%")
+    while True:
+        # Obter uso do sistema
+        cpu, ram, disk = get_system_usage()
+        
+        # Registrar uso do sistema no CSV
+        relatorio.write_performance(cpu, ram, disk)
+        
+        # Obter e registrar lista de processos no CSV
+        process_list = list_processes()
+        relatorio.write_processes(process_list)
 
-		time.sleep(5)
+        # Print para visualização (opcional)
+        print(f"Uso da CPU: {cpu}%")
+        print(f"Uso da RAM: {ram}%")
+        print(f"Uso do Disco: {disk}%")
+        for process in process_list:
+            print(f"PID: {process['pid']}, Nome: {process['name']}, Usuario: {process['username']}, CPU: {process['cpu_percent']}, Memoria: {process['memory_percent']}%")
+        
+        time.sleep(5)
