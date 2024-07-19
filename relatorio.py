943e15bd5d4e3d664ede2d9624bcc649afc1c210
d3v1lcon3 <135651153+d3v1lcon3@users.noreply.github.com>
2024-07-19 15:39:17 -0300

    Update relatorio.py versao 4

--- relatorio.py
+++ relatorio.py
@@ -11,26 +11,28 @@ data_processes_name = f'processes_{data}.csv'
 # Variáveis globais para os escritores
 writer_performance = None
 writer_processes = None
+file_performance = None
+file_processes = None
 
 # Abrir os arquivos
 def open_tables():
-    global writer_performance, writer_processes
+    global writer_performance, writer_processes, file_performance, file_processes
 
     # Verificar se os arquivos existem
     performance_exist = os.path.isfile(data_performance_name)
     processes_exist = os.path.isfile(data_processes_name)
 
     # Abrir e configurar o arquivo de performance
-    with open(data_performance_name, mode='a' if performance_exist else 'w', newline='', encoding='utf-8') as file:
-        writer_performance = csv.DictWriter(file, fieldnames=['CPU', 'RAM', 'Disk'])
-        if not performance_exist:
-            writer_performance.writeheader()
+    file_performance = open(data_performance_name, mode='a' if performance_exist else 'w', newline='', encoding='utf-8')
+    writer_performance = csv.DictWriter(file_performance, fieldnames=['CPU', 'RAM', 'Disk'])
+    if not performance_exist:
+        writer_performance.writeheader()
 
     # Abrir e configurar o arquivo de processos
-    with open(data_processes_name, mode='a' if processes_exist else 'w', newline='', encoding='utf-8') as file:
-        writer_processes = csv.DictWriter(file, fieldnames=['PID', 'Nome', 'Usuario', 'CPU', 'Memoria'])
-        if not processes_exist:
-            writer_processes.writeheader()
+    file_processes = open(data_processes_name, mode='a' if processes_exist else 'w', newline='', encoding='utf-8')
+    writer_processes = csv.DictWriter(file_processes, fieldnames=['PID', 'Nome', 'Usuario', 'CPU', 'Memoria'])
+    if not processes_exist:
+        writer_processes.writeheader()
 
 # Registrar os dados de performance
 def write_performance(cpu, ram, disk):
@@ -42,3 +44,10 @@ def write_processes(procs):
     if writer_processes:
         for proc in procs:
             writer_processes.writerow(proc)
+
+# Fechar os arquivos
+def close_tables():
+    if file_performance:
+        file_performance.close()
+    if file_processes:
+        file_processes.close()
