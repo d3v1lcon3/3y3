39a9c4f3965b83994d53b0b161e60f3afc24e1dc
d3v1lcon3 <135651153+d3v1lcon3@users.noreply.github.com>
2024-07-22 15:12:26 -0300

    adicionada a coluna 'horas' nas tabelas

--- dados.py
+++ dados.py
@@ -2,6 +2,7 @@
 
 import psutil
 import time
+from datetime import datetime
 import relatorio
 
 def list_processes():
@@ -28,26 +29,32 @@ if __name__ == "__main__":
 
     try:
         while True:
+            # Captura a hora atual
+            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
             # Obter uso do sistema
             cpu, ram, disk = get_system_usage()
             
-            # Registrar uso do sistema no CSV
-            relatorio.write_performance(cpu, ram, disk)
+            # Registrar uso do sistema no CSV com a hora atual
+            relatorio.write_performance(current_time, cpu, ram, disk)
             # Forçar gravação no disco
             relatorio.file_performance.flush()
             
-            # Obter e registrar lista de processos no CSV
+            # Obter e registrar lista de processos no CSV com a hora atual
             process_list = list_processes()
+            for process in process_list:
+                process['Hora'] = current_time
             relatorio.write_processes(process_list)
             # Forçar gravação no disco
             relatorio.file_processes.flush()
 
             # Print para visualização (opcional)
+            print(f"Hora: {current_time}")
             print(f"Uso da CPU: {cpu}%")
             print(f"Uso da RAM: {ram}%")
             print(f"Uso do Disco: {disk}%")
             for process in process_list:
-                print(f"PID: {process['pid']}, Nome: {process['name']}, Usuario: {process['username']}, CPU: {process['cpu_percent']}, Memoria: {process['memory_percent']}%")
+                print(f"Hora: {process['Hora']}, PID: {process['pid']}, Nome: {process['name']}, Usuario: {process['username']}, CPU: {process['cpu_percent']}%, Memoria: {process['memory_percent']}%")
             
             time.sleep(5)
     except KeyboardInterrupt:
