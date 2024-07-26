05c07cbb1cd193bee6d63a7ee687a3993683c84c
d3v1lcon3 <135651153+d3v1lcon3@users.noreply.github.com>
2024-07-26 10:19:33 -0300

    Update relatorio.py
    - acrescentou recolhimentos de dados de networking da performanve
    -acrescentou recolhimento de dados de networking e disco dos processos individuais

--- relatorio.py
+++ relatorio.py
@@ -12,22 +12,22 @@ def open_tables():
     # Escrita do cabeçalho se o arquivo estiver vazio
     if file_performance.tell() == 0:
         performance_writer = csv.writer(file_performance)
-        performance_writer.writerow(['Hora', 'CPU', 'RAM', 'Disk'])
+        performance_writer.writerow(['Hora', 'CPU', 'RAM', 'Disk', 'Net Sent', 'Net Recv'])
     
     if file_processes.tell() == 0:
         processes_writer = csv.writer(file_processes)
-        processes_writer.writerow(['Hora', 'PID', 'Nome', 'Usuario', 'CPU (%)', 'Memoria (%)'])
+        processes_writer.writerow(['Hora', 'PID', 'Nome', 'Usuario', 'CPU (%)', 'Memoria (%)', 'Disco Lido (bytes)', 'Disco Escrito (bytes)', 'Rede Enviada (bytes)', 'Rede Recebida (bytes)'])
 
 # Função para registrar o uso do sistema no CSV com a hora atual
-def write_performance(current_time, cpu, ram, disk):
+def write_performance(current_time, cpu, ram, disk, net_sent, net_recv):
     performance_writer = csv.writer(file_performance)
-    performance_writer.writerow([current_time, cpu, ram, disk])
+    performance_writer.writerow([current_time, cpu, ram, disk, net_sent, net_recv])
 
 # Função para registrar a lista de processos no CSV com a hora atual
 def write_processes(process_list):
     processes_writer = csv.writer(file_processes)
     for process in process_list:
-        processes_writer.writerow([process['Hora'], process['pid'], process['name'], process['username'], process['cpu_percent'], process['memory_percent']])
+        processes_writer.writerow([process['Hora'], process['pid'], process['name'], process['username'], process['cpu_percent'], process['memory_percent'], process['disk_read_bytes'], process['disk_write_bytes'], process['net_bytes_sent'], process['net_bytes_recv']])
 
 # Fechar os arquivos CSV (exemplo)
 def close_tables():
