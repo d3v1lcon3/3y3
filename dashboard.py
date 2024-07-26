7fd571859e89c3233403ff39c1891de10fcfc47a
d3v1lcon3 <135651153+d3v1lcon3@users.noreply.github.com>
2024-07-26 10:42:56 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,6 +5,7 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
 import os
+from datetime import datetime
 
 # Função para obter o nome do arquivo de log de performance atual
 def get_log_filename():
